a1152b52ad7ab21394a433104d2f8e97fe9d275f
IvyMtsh <147334689+IvyMtsh@users.noreply.github.com>
2024-01-19 20:43:59 +0200

    update task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,7 @@
 function formatMessage(message, maxLength) {
-  if (maxLength >= message.length) {
-    return message.slice(0, maxLength) + '...';
-  } else {
-    return message;
-  }
+  return message.length <= maxLength
+    ? message
+    : message.slice(0, maxLength) + '...';
 }
 console.log(formatMessage('Curabitur ligula sapien', 16)); // "Curabitur ligula..."
 console.log(formatMessage('Curabitur ligula sapien', 23)); // "Curabitur ligula sapien"
